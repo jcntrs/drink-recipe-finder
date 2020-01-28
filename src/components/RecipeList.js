5610927b1283f22a925fd6855db7ee236e24b8e6
Jean C. Contreras <jean.cntrs@gmail.com>
2020-01-28 18:50:13 -0300

    Creating modal component with materia UI

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,8 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { RecipesContext } from '../context/RecipesContext';
 import { ModalContext } from '../context/ModalContext';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+const getModalStyle = () => {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const RecipeList = () => {
+
+
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
 
     const { recipes } = useContext(RecipesContext);
     const { setRecipeId } = useContext(ModalContext);
@@ -13,16 +49,29 @@ const RecipeList = () => {
                 <div className="col-md-4 mb-3" key={element.idDrink}>
                     <div className="card">
                         <h2 className="card-header"><strong>{element.strDrink}</strong></h2>
-                        <img className="card-img-top" src={element.strDrinkThumb} alt={`Omagen de ${element.strDrink}`} />
+                        <img className="card-img-top" src={element.strDrinkThumb} alt={`Imagen de ${element.strDrink}`} />
                         <div className="card-body">
-                            <button className="btn btn-block btn-outline-warning" onClick={() => setRecipeId(element.idDrink)}><strong>Ver Receta</strong></button>
+                            <button className="btn btn-block btn-outline-warning" onClick={() => {
+                                setRecipeId(element.idDrink);
+                                handleOpen();
+                            }}>
+                                <strong>Ver Receta</strong>
+                            </button>
                         </div>
                     </div>
                 </div>
             )}
+            <Modal open={open} onClose={() => {
+                setRecipeId(null);
+                handleClose();
+            }}>
+                <div className={classes.paper} style={modalStyle}>
+                    <h1>From Modal</h1>
+                </div>
+            </Modal>
         </div>
     );
-    
+
 }
 
 export default RecipeList;
