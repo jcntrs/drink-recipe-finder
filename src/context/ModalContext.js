b21a34f07dd063369e60701a3dfa2fd1064aba26
Jean C. Contreras <jean.cntrs@gmail.com>
2020-01-28 13:20:29 -0300

    Consulting API for recipe detail

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -6,13 +6,25 @@ export const ModalContext = createContext();
 const ModalProvider = props => {
 
     const [recipeId, setRecipeId] = useState(null);
+    const [recipeDetail, setRecipeDetail] = useState({});
+
+    useEffect(() => {
+        if (recipeId !== null) {
+            const getRecipeDetail = async () => {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
+                const result = await axios.get(url);
+                setRecipeDetail(result.data.drinks[0]);
+            }
+            getRecipeDetail();
+        }
+    }, [recipeId]);
 
     return (
         <ModalContext.Provider value={{ setRecipeId }}>
             {props.children}
         </ModalContext.Provider>
     );
-    
+
 }
 
 export default ModalProvider;
