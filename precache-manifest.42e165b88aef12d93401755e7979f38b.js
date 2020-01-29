8c9fc52c2c7afceae0360fb15a519d1433b3360b
Jean C. Contreras <jean.cntrs@gmail.com>
2020-01-29 16:40:51 -0300

    Updates

---
+++ precache-manifest.42e165b88aef12d93401755e7979f38b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb96e74e152bc8f74fb5cffe406bddd8",
+    "url": "/drink-recipe-finder/index.html"
+  },
+  {
+    "revision": "bd036112f50e875dec2f",
+    "url": "/drink-recipe-finder/static/js/2.fd341d3d.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/drink-recipe-finder/static/js/2.fd341d3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7cb483e6ca1caed4056",
+    "url": "/drink-recipe-finder/static/js/main.c570ad67.chunk.js"
+  },
+  {
+    "revision": "1f7bdebafef243ea97cc",
+    "url": "/drink-recipe-finder/static/js/runtime-main.c3d75a05.js"
+  }
+]);
